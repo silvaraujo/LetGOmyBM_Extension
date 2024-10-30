27f0a1a8d5e2da4b32ba5c274cf0a197d9db88cb
Caio R. S. de Araújo <159500703+silvaraujo@users.noreply.github.com>
2024-10-30 10:47:43 -0300

    :sparkles: feat: Adicionando a parte de tickets

--- Solte meu BM! by Silvaraujo/background.js
+++ Solte meu BM! by Silvaraujo/background.js
@@ -4,11 +4,13 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
+//links com acesso a extensão
 const report = 'https://app.buzzmonitor.com.br/reports';
+const ticket = 'https://app.buzzmonitor.com.br/tickets'
 
 //gerenciador de estado de atividade da extensão
 chrome.action.onClicked.addListener(async (tab) => {
-    if ( tab.url.startsWith(report) ) {
+    if ( tab.url.startsWith(report) || tab.url.startsWith(ticket) ) {
 
         const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
 
